c9a8864aca5d0f9af9a3bccb418fcf9f4a647367
DANI <danisetiawan21057@gmail.com>
2025-04-03 07:39:53 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Rest API DanX adalah proyek yang menyediakan berbagai endpoint API untuk berbagai keperluan. Dibangun menggunakan Node.js dan Express.js, proyek ini dirancang untuk memudahkan pengembangan aplikasi dengan akses data yang mudah.
+
+## Link API
+Anda dapat mengakses Rest API di sini: [https://api.ownblox.biz.id](https://api.ownblox.biz.id)
+# Menggunakan base dari Rest API OwnBlox
+## Lisensi
+Proyek ini menggunakan MIT License.
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+let totalRequests = 0;
+let clients = [];
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    totalRequests++;
+    sendUpdateToClients();
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/monitor-page", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "monitor", "monitor.html"));
+});
+
+app.get("/monitor", (req, res) => {
+    res.set({
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+    });
+    res.flushHeaders();
+
+    const clientId = Date.now();
+    const newClient = { id: clientId, res };
+    clients.push(newClient);
+
+    res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+
+    req.on("close", () => {
+        clients = clients.filter(client => client.id !== clientId);
+    });
+});
+
+function sendUpdateToClients() {
+    clients.forEach(client => {
+        client.res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+    });
+}
+const port = process.env.PORT || 2503;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server berjalan di port ${port}`);
+});
+
+const routes = ["ytdl", "twitterdl", "igdl", "fbdl", "ttdl", "githubstalk", "searchgroups", "ttsearch", "ytsearch", "llama-3.3-70b-versatile", "txt2img", "ssweb", "translate", "nulis", "cekkhodam", "tahukahkamu", "brat", "qc", "detiknews", "deepseek-70b", "qwenai-32b", "anime-terbaru"];
+routes.forEach(route => {
+    app.use(`/api/${route}`, require(`./api/${route}`));
+});
+
+module.exports = app;
